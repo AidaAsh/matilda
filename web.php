033df39a576415eac206b67050d2ad71406bf700
zhanna <61663127+R-e-y@users.noreply.github.com>
2020-04-17 19:01:16 +0600

    Add files via upload
    папка view и web.php

--- web.php
+++ web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Course;
+use Illuminate\Http\Request;
+
+Route::auth();
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,3 +27,26 @@ Route::post('/workers/add', 'WorkerController@store');
 Route::get('/workers/add', 'WorkerController@create');
 
 Route::delete('/workers/delete/{worker}', 'WorkerController@destroy');
+
+
+Route::get('/workers/edit/{worker}', 'WorkerController@edit');
+
+Route::patch('/workers/update/{worker}', 'WorkerController@update');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
+{
+Route::match(['get', 'post'], '/adminOnlyPage/', 'WorkerController@admin');
+});
+
+// Route::get('/courses', 'CourseController@index')->name('courses')->middleware(StudentMiddleware::class);
+
+
+Route::group(['middleware' => 'App\Http\Middleware\AccountantMiddleware'], function()
+{
+Route::match(['get', 'post'], '/accountantOnlyPage/', 'WorkerController@accountant');
+});
